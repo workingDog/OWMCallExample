3ab24943a7c8fe3b3967529fde1fd98ef62e2d81
workingdog <ringowathelet@gmail.com>
2022-10-01 14:58:05 +0900

    info update

--- OWMCallExample/ContentView.swift
+++ OWMCallExample/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
             loadData()
         }
         //        .task {
-        //            if let results = await weatherProvider.getWeather(lat: 35.661991, lon: 139.762735, options: OWOptions(excludeMode: [], units: .metric, lang: "en")) {
+        //            if let results = await weatherProvider.getWeather(lat: 35.661991, lon: 139.762735, options: OWOptions(units: .metric, lang: "en")) {
         //                weather = results
         //            }
         //        }
@@ -47,21 +47,17 @@ struct ContentView: View {
     func loadData() {
         // lat: -33.861536, lon: 151.215206,    // Sydney
         // lat: 35.661991, lon: 139.762735,     // Tokyo
-        
-        let myOptions = OWMOptions(units: .metric, lang: "en")
-        
+
         // using a binding
-        weatherProvider.getWeather(lat: 35.661991, lon: 139.762735, weather: $weather, options: myOptions)
+        weatherProvider.getWeather(lat: 35.661991, lon: 139.762735, weather: $weather, options: OWMOptions.metric())
         
         // closure style callback
-        //        weatherProvider.getWeather(lat: 35.661991, lon: 139.762735, options: myOptions) { response in
+        // let myOptions = OWMOptions(units: .metric, lang: "en")
+        // weatherProvider.getWeather(lat: 35.661991, lon: 139.762735, options: myOptions) { response in
         //            if let theWeather = response {
         //                self.weather = theWeather
         //            }
         //        }
-        
-        // for historical data in the past
-        //         weatherProvider.getWeather(lat: 35.661991, lon: 139.762735, weather: $weather, options: OWHistOptions.yesterday())
     }
     
     func formattedDate(utc: Int) -> String {
