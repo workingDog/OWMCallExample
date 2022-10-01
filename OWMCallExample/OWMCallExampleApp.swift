70c691b67a3f4840e5b122d285283413985cd585
workingdog <ringowathelet@gmail.com>
2022-10-01 14:24:34 +0900

    Initial Commit

---
+++ OWMCallExample/OWMCallExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OWMCallExampleApp.swift
+//  OWMCallExample
+//
+//  Created by Ringo Wathelet on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct OWMCallExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
